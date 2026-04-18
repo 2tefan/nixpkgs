a778ca2928feb69a009d9bac7bf8640ffaf5e6ce
2tefan <me@2tefan.eu>
2026-04-18 20:16:17 +0200

    Play around a bit w/ tests for mosquitto

--- pkgs/by-name/mo/mosquitto/package.nix
+++ pkgs/by-name/mo/mosquitto/package.nix
@@ -5,13 +5,19 @@
   cmake,
   docbook_xsl,
   libxslt,
+  cunit,
+  gtest,
   c-ares,
   cjson,
+  libargon2,
   libuuid,
   libuv,
   libwebsockets,
   openssl,
+  python3,
+  python3Packages,
   withSystemd ? lib.meta.availableOn stdenv.hostPlatform systemd,
+  sqlite,
   systemd,
   uthash,
   nixosTests,
@@ -32,20 +38,55 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "mosquitto";
-  version = "2.0.22";
+  version = "2.1.2";
+  doCheck = true; # upstream tests require
 
   src = fetchFromGitHub {
-    owner = "eclipse";
+    owner = "eclipse-mosquitto";
     repo = "mosquitto";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-PCiNxRG2AqVlS2t/u7Cqn8NbTrrYGO1OXl8zvPQRrJM=";
+    hash = "sha256-Zl55yjuzQY2fyaKs/zLaJ7a3OONKTDQPaT+DpPURdZI=";
   };
 
+prePatch = ''
+  echo "=== checking test files ==="
+  ls -R test/apps || true
+    ls -l test/apps/ctrl/ctrl-args.py || true
+  head -n10 test/apps/ctrl/ctrl-args.py || true
+  pwd
+  command -v python3 || true
+  command -v env || true
+'';
+checkPhase = ''
+  runHook preCheck
+  echo test-override
+  ctest --output-on-failure --verbose  -j1
+  runHook postCheck
+'';
   postPatch = ''
     for f in html manpage ; do
       substituteInPlace man/$f.xsl \
         --replace http://docbook.sourceforge.net/release/xsl/current ${docbook_xsl}/share/xml/docbook-xsl
     done
+
+    patchShebangs test
+
+
+    substituteInPlace test/apps/passwd/passwd-changes.py \
+      --replace-fail 'except PermissionError:' \
+                     'except OSError:'
+    substituteInPlace test/apps/ctrl/ctrl-dynsec.py \
+      --replace-fail 'except PermissionError:' \
+                     'except OSError:'
+  substituteInPlace test/apps/signal/signal-args.py \
+    --replace-fail 'do_test(["-p", "1", "config-reload"], 0)
+do_test(["-p", "1", "log-rotate"], 0)
+do_test(["-p", "1", "shutdown"], 0)
+do_test(["-p", "1", "tree-print"], 0)
+do_test(["-p", "1", "xtreport"], 0)
+do_test(["-a", "config-reload"], 0)
+' \'\'
+
   '';
 
   outputs = [
@@ -58,15 +99,22 @@ stdenv.mkDerivation (finalAttrs: {
     cmake
     docbook_xsl
     libxslt
+  ] ++ lib.optionals finalAttrs.doCheck [
+    cunit
+    gtest
+    python3
+    python3Packages.psutil
   ];
 
   buildInputs = [
     c-ares
     cjson
+    libargon2
     libuuid
     libuv
     libwebsockets'
     openssl
+    sqlite
     uthash
   ]
   ++ lib.optional withSystemd systemd;
@@ -75,10 +123,11 @@ stdenv.mkDerivation (finalAttrs: {
     (lib.cmakeBool "WITH_BUNDLED_DEPS" false)
     (lib.cmakeBool "WITH_WEBSOCKETS" true)
     (lib.cmakeBool "WITH_SYSTEMD" withSystemd)
+    (lib.cmakeBool "WITH_TESTS" finalAttrs.doCheck)
   ];
 
   postFixup = ''
-    sed -i "s|^prefix=.*|prefix=$lib|g" $dev/lib/pkgconfig/*.pc
+    sed -i "s|^libdir=.*|libdir=$lib/lib|g" $dev/lib/pkgconfig/*.pc
   '';
 
   passthru.tests = {
